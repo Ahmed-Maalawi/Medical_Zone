3810f5837c1856bd60d269bc1af351fa669b533c
Ahmed El-Maalawi <ahmedelmaalawi@gmail.com>
2022-05-08 22:33:06 +0200

    Add the basic component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { CartComponent } from './component/cart/cart.component';
+import { AboutComponent } from './component/about/about.component';
+import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './component/contact/contact.component';
+import { Page404Component } from './component/page404/page404.component';
+import { WishlistComponent } from './component/wishlist/wishlist.component';
+import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+  {
+    path: 'Home',
+    children: [
+      {path: '', component: HomeComponent},
+      {path: 'About-Us', component: AboutComponent},
+      {path: 'Contact', component: ContactComponent},
+      {path: 'Cart', component: CartComponent},
+      {path: 'Wishlist', component: WishlistComponent},
+      {path: 'Login', component: LoginComponent},
+    ],
+  },
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { CartComponent } from './component/cart/cart.component';
+import { WishlistComponent } from './component/wishlist/wishlist.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { Page404Component } from './component/page404/page404.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CartComponent,
+    WishlistComponent,
+    AboutComponent,
+    ContactComponent,
+    Page404Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
